423cbe29fdd70d4b705c47645ec173af2b89832e
jacksonbryan710 <jacksonbryan710@gmail.com>
2026-02-21 11:33:13 -0500

    uibuild

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -1,79 +1,68 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
-const morgan = require('morgan');
-const Note = require('./models/note')
-const app = express();
-app.use(express.json());
-app.use(express.static('dist'));
+const Note = require('./models/note');
 
-morgan.token('body', function (req, res) {
-    return [JSON.stringify(req.body), JSON.stringify(res.body)];
+const app = express();
+
+let notes = [];
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method);
+    console.log('Path:  ', request.path);
+    console.log('Body:  ', request.body);
+    console.log('---');
+    next();
+};
+
+app.use(requestLogger);
+app.use(express.static('dist'));
+app.use(express.json());
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>');
 });
 
-app.use(
-    morgan(function (tokens, req, res) {
-        return [
-            tokens.method(req, res),
-            tokens.url(req, res),
-            tokens.status(req, res),
-            tokens.res(req, res, 'content-length'),
-            '-',
-            tokens['response-time'](req, res),
-            'ms',
-            tokens.body(req, res),
-        ].join(' ');
-    })
-);
-
 app.get('/api/notes', (request, response) => {
-    Note.find({}).then(notes => {
-        response.json(notes)
-    })
+    Note.find({}).then((notes) => {
+        response.json(notes);
+    });
 });
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id;
-    const note = notes.find((note) => note.id === id);
-
-    if (note) {
+    Note.findById(request.params.id).then((note) => {
         response.json(note);
-    } else {
-        response.status(404).end();
-    }
-});
-
-app.put('/api/notes/:id', (request, response) => {
-    const id = request.params.id;
-    const updatedNote = request.body;
-    notes = notes.map((note) => (note.id === id ? updatedNote : note));
-
-    response.json(updatedNote);
-});
-
-app.delete('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => {
-        response.json(note)
-    })
+    });
 });
 
 app.post('/api/notes', (request, response) => {
     const body = request.body;
 
     if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing',
-        });
+        return response.status(400).json({ error: 'content missing' });
     }
 
-    const note = new Note ({
+    const note = new Note({
         content: body.content,
         important: body.important || false,
     });
 
-    note.save().then(savedNote => {
-        response.json(savedNote)
-    })
+    note.save().then((savedNote) => {
+        response.json(savedNote);
+    });
 });
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = request.params.id;
+    notes = notes.filter((note) => note.id !== id);
+
+    response.status(204).end();
+});
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
--- notes-backend/models/note.js
+++ notes-backend/models/note.js
@@ -6,8 +6,7 @@ const url = process.env.MONGODB_URI;
 
 console.log('connecting to', url);
 mongoose
-    .connect(url, { family: 4 })
-
+    .connect(url)
     .then((result) => {
         console.log('connected to MongoDB');
     })
