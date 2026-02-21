ab67052c0f2bec683df453d1cf2c0531c4952105
jacksonbryan710 <jacksonbryan710@gmail.com>
2026-02-21 11:26:05 -0500

    uibuild

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -1,12 +1,14 @@
+require('dotenv').config()
 const express = require('express');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const Note = require('./models/note')
 const app = express();
 app.use(express.json());
 app.use(express.static('dist'));
 
 morgan.token('body', function (req, res) {
-    return [JSON.stringify(req.body), JSON.stringify(res.body)]
-})
+    return [JSON.stringify(req.body), JSON.stringify(res.body)];
+});
 
 app.use(
     morgan(function (tokens, req, res) {
@@ -23,30 +25,10 @@ app.use(
     })
 );
 
-let notes = [
-    {
-        id: '1',
-        content: 'HTML is easy',
-        important: true,
-    },
-    {
-        id: '2',
-        content: 'Browser can execute only JavaScript',
-        important: false,
-    },
-    {
-        id: '3',
-        content: 'GET and POST are the most important methods of HTTP protocol',
-        important: true,
-    },
-];
-
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>');
-});
-
 app.get('/api/notes', (request, response) => {
-    response.json(notes);
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 });
 
 app.get('/api/notes/:id', (request, response) => {
@@ -63,22 +45,16 @@ app.get('/api/notes/:id', (request, response) => {
 app.put('/api/notes/:id', (request, response) => {
     const id = request.params.id;
     const updatedNote = request.body;
-    notes = notes.map(note => note.id === id ? updatedNote : note)
+    notes = notes.map((note) => (note.id === id ? updatedNote : note));
 
-    response.json(updatedNote)
-})
-
-app.delete('/api/notes/:id', (request, response) => {
-    const id = request.params.id;
-    notes = notes.filter((note) => note.id !== id);
-
-    response.status(204).end();
+    response.json(updatedNote);
 });
 
-const generateId = () => {
-    const maxId = notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
-    return String(maxId + 1);
-};
+app.delete('/api/notes/:id', (request, response) => {
+    Note.findById(request.params.id).then(note => {
+        response.json(note)
+    })
+});
 
 app.post('/api/notes', (request, response) => {
     const body = request.body;
@@ -89,18 +65,17 @@ app.post('/api/notes', (request, response) => {
         });
     }
 
-    const note = {
+    const note = new Note ({
         content: body.content,
         important: body.important || false,
-        id: generateId(),
-    };
+    });
 
-    notes = notes.concat(note);
-
-    response.json(note);
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 });
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
